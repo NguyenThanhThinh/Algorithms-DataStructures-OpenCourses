c253c955da8869402d4731f1af3e95699908e40c
evgeni-tsn <evgeni.tsv.nikolov@gmail.com>
2016-02-20 18:17:56 +0200

    Doubly Linked List Exercise

---
+++ 02.Doubly-Linked-List-Exercise/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,130 @@
+﻿namespace Double_Linked_List
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class DoublyLinkedList<T> : IEnumerable<T>
+    {
+        private ListNode<T> head;
+
+        private ListNode<T> tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode<T>(element);
+            }
+            else
+            {
+                var newHead = new ListNode<T>(element);
+                newHead.NextNode = this.head;
+                this.head.PrevNode = newHead;
+                this.head = newHead;
+            }
+
+            this.Count++;
+        }
+
+        public void AddLast(T element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode<T>(element);
+            }
+            else
+            {
+                var newTail = new ListNode<T>(element);
+                newTail.PrevNode = this.tail;
+                this.tail.NextNode = newTail;
+                this.tail = newTail;
+            }
+
+            this.Count++;
+        }
+
+        public T RemoveFirst()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("List empty");
+            }
+
+            var firstElement = this.head.Value;
+            this.head = this.head.NextNode;
+            if (this.head != null)
+            {
+                this.head.PrevNode = null;
+            }
+            else
+            {
+                this.tail = null;
+            }
+
+            this.Count--;
+            return firstElement;
+        }
+
+        public T RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("List empty");
+            }
+
+            var lastElement = this.tail.Value;
+            this.tail = this.tail.PrevNode;
+            if (this.tail != null)
+            {
+                this.tail.NextNode = null;
+            }
+            else
+            {
+                this.head = null;
+            }
+
+            this.Count--;
+            return lastElement;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                yield return currentNode.Value;
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        public T[] ToArray()
+        {
+            var arr = new T[this.Count];
+            int index = 0;
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                arr[index] = currentNode.Value;
+                currentNode = currentNode.NextNode;
+                index++;
+            }
+
+            return arr;
+        }
+    }
+}
