3c45965b1639dfc6af9f766003e1b0b0ce9077c0
evgeni-tsn <evgeni.tsv.nikolov@gmail.com>
2016-03-18 22:58:54 +0200

    AVL Tree Implementation Exercise

---
+++ 08.AvlTree-Exercise/AvlTreeLab/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AvlTreeLab
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tree = new AvlTree<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                tree.Add(1);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
