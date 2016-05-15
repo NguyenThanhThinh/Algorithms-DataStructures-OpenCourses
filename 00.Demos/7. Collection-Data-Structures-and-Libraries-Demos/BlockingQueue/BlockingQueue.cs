c0dfead9b5dfc453c77a8234b520b16dae473292
evgeni-tsn <evgeni.tsv.nikolov@gmail.com>
2016-05-16 00:58:37 +0300

    Added Useful Ready To Use Demos

---
+++ 00.Demos/7. Collection-Data-Structures-and-Libraries-Demos/BlockingQueue/BlockingQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+class BlockingQueue
+{
+    //static CustomBlockingQueue<string> tasks = new CustomBlockingQueue<string>();
+    static BlockingCollection<string> tasks = new BlockingCollection<string>();
+
+    private static void Consumer()
+    {
+        while (true)
+        {
+            var task = tasks.Take();
+            Console.WriteLine("Consumer {1} is executing task {0} ...", task, Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(2000);
+            Console.WriteLine("Consumer {1} ready - Task {0} finished.", task, Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+
+    static void Main()
+    {
+        // Start 3 consumers (task executors)
+        for (int i = 0; i < 3; i++)
+            (new Thread(Consumer)).Start();
+
+        // Start the producer
+        while (true)
+        {
+            Console.WriteLine("Press [Enter] to produce new task");
+            Console.ReadLine();
+            var task = "Task " + DateTime.Now.Ticks;
+            tasks.Add(task);
+        }
+    }
+}
---
+++ 00.Demos/7. Collection-Data-Structures-and-Libraries-Demos/BlockingQueue/CustomBlockingQueue.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+public class CustomBlockingQueue<T>
+{
+    private readonly Queue<T> queue = new Queue<T>();
+    private readonly int capacity;
+
+    public CustomBlockingQueue(int capacity = 16)
+    {
+        this.capacity = capacity;
+    }
+
+    public void Add(T item)
+    {
+        lock (queue)
+        {
+            while (queue.Count >= this.capacity)
+            {
+                // Release lock and sleep until another thread wakes you up
+                Monitor.Wait(queue);
+            }
+
+            queue.Enqueue(item);
+
+            if (queue.Count == 1)
+            {
+                // Wake up any blocked dequeue
+                Monitor.PulseAll(queue);
+            }
+        }
+    }
+
+    public T Take()
+    {
+        lock (queue)
+        {
+            while (queue.Count == 0)
+            {
+                Monitor.Wait(queue);
+            }
+
+            T item = queue.Dequeue();
+
+            if (queue.Count == this.capacity - 1)
+            {
+                // wake up any blocked enqueue
+                Monitor.PulseAll(queue);
+            }
+
+            return item;
+        }
+    }
+}
